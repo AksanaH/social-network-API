507563bd1c5c1417088a1bdae7351166609482e3
Aksana <aksa.hleb@gmail.com>
2024-06-21 16:36:21 -0500

    Initial commit

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    thoughtText: {
+        type: String,
+        required: true,
+        trim: true,
+        max_length: 280,
+        min_length: 1,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function (timestamp) {
+            return moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
+        }
+    },
+    username: {
+        type: String,
+        required: true
+    }
+    ,
+    reactions: [reactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
